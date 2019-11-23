f22980753f928bd27296189e0ccbdac2f55cf4d0
toandaominh1997 <toandaominh1997@gmail.com>
2019-11-23 10:52:15 +0700

    fix ncols, num image

--- demo.py
+++ demo.py
@@ -8,31 +8,42 @@ img1 = Image.open('/home/toandm2/Downloads/test.jpg')
 img2 = cv2.imread('/home/toandm2/Downloads/test.jpg')
 
 
-def visualization(*kwarg, title=None, ncols=2):
+def imshow(*kwarg, title=None, ncols=2, figsize=(10, 10)):
     num_image = len(kwarg)
-    f, ax = plt.subplots(math.ceil(num_image/2.0), ncols, figsize=(10, 10))
-    print(ax.shape)
+    if ncols<1:
+        print('WARNING: Columns must greater than 0')
+        return None
+    if num_image < 1:
+        print('WARNING: Number of image must greater than 0')
+    
+    if ncols ==1 or num_image==1:
+        plt.figure()
+        plt.imshow(kwarg[0])
+        return True 
+    if ncols==1 or num_image==1:
+        f, ax = plt.subplots(max(ncols, num_image), figsize=figsize)
+    else:
+        f, ax = plt.subplots(math.ceil(num_image/2.0), ncols, figsize=figsize)
     for idx, img in enumerate(kwarg):
         img = np.array(img, dtype = np.uint8)
-        if num_image <= ncols:
+        if ncols==1 or num_image==1:
             ax[int(idx%2)].imshow(img)
         else:
             ax[int(idx/2), int(idx%2)].imshow(img)
         if title is not None:
             try:
-                if num_image <= ncols:
+                if ncols==1 or num_image==1:
                     ax[int(idx%2)].set_title(title[idx])
                 else:
                     ax[int(idx/2), int(idx%2)].set_title(title[idx])
             except IndexError:
-                if num_image <= ncols:
+                if ncols==1 or num_image==1:
                     ax[int(idx%2)].set_title('Figure: {}'.format(idx))
                 else:
                     ax[int(idx/2), int(idx%2)].set_title('Figure {}'.format(idx))
+# from pytoan.pyplot import imshow
 
-from pytoan.pyplot import imshow
-
-imshow(img1, img1)
+imshow(img1, ncols=2)
 
 # plt.imshow(img)
 plt.show()
--- pytoan/pyplot/show.py
+++ pytoan/pyplot/show.py
@@ -4,22 +4,34 @@ import math
 
 def imshow(*kwarg, title=None, ncols=2, figsize=(10, 10)):
     num_image = len(kwarg)
-    f, ax = plt.subplots(math.ceil(num_image/2.0), ncols, figsize=(10, 10))
-    print(ax.shape)
+    if ncols<1:
+        print('WARNING: Columns must greater than 0')
+        return None
+    if num_image < 1:
+        print('WARNING: Number of image must greater than 0')
+    
+    if ncols ==1 and num_image==1:
+        plt.figure()
+        plt.imshow(kwarg[0])
+        return True 
+    if ncols==1 or num_image==1:
+        f, ax = plt.subplots(max(ncols, num_image), figsize=figsize)
+    else:
+        f, ax = plt.subplots(math.ceil(num_image/2.0), ncols, figsize=figsize)
     for idx, img in enumerate(kwarg):
         img = np.array(img, dtype = np.uint8)
-        if num_image <= ncols:
+        if ncols==1 or num_image==1:
             ax[int(idx%2)].imshow(img)
         else:
             ax[int(idx/2), int(idx%2)].imshow(img)
         if title is not None:
             try:
-                if num_image <= ncols:
+                if ncols==1 or num_image==1:
                     ax[int(idx%2)].set_title(title[idx])
                 else:
                     ax[int(idx/2), int(idx%2)].set_title(title[idx])
             except IndexError:
-                if num_image <= ncols:
+                if ncols==1 or num_image==1:
                     ax[int(idx%2)].set_title('Figure: {}'.format(idx))
                 else:
                     ax[int(idx/2), int(idx%2)].set_title('Figure {}'.format(idx))
