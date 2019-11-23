fc4c1b7433973ac0bf6186f70292966106b2e883
toandaominh1997 <toandaominh1997@gmail.com>
2019-11-23 11:12:17 +0700

    Complete imshow

--- pytoan/pyplot/show.py
+++ pytoan/pyplot/show.py
@@ -1,37 +1,24 @@
-import numpy as np 
-import matplotlib.pyplot as plt 
+import numpy as np
+import matplotlib.pyplot as plt
 import math
+
 
 def imshow(*kwarg, title=None, ncols=2, figsize=(10, 10)):
     num_image = len(kwarg)
-    if ncols<1:
-        print('WARNING: Columns must greater than 0')
+    if ncols < 1:
+        print('WARNING: Columns must greater than 0. Return None')
         return None
     if num_image < 1:
-        print('WARNING: Number of image must greater than 0')
-    
-    if ncols ==1 and num_image==1:
-        plt.figure()
-        plt.imshow(kwarg[0])
-        return True 
-    if ncols==1 or num_image==1:
-        f, ax = plt.subplots(max(ncols, num_image), figsize=figsize)
-    else:
-        f, ax = plt.subplots(math.ceil(num_image/2.0), ncols, figsize=figsize)
+        print('WARNING: Number of image must greater than 0. Return None')
+        return None
+    f, ax = plt.subplots(math.ceil(num_image/ncols), ncols,
+                         figsize=figsize, squeeze=False)
     for idx, img in enumerate(kwarg):
-        img = np.array(img, dtype = np.uint8)
-        if ncols==1 or num_image==1:
-            ax[int(idx%2)].imshow(img)
-        else:
-            ax[int(idx/2), int(idx%2)].imshow(img)
+        img = np.array(img, dtype=np.uint8)
+        ax[int(idx/ncols), int(idx % ncols)].imshow(img)
         if title is not None:
             try:
-                if ncols==1 or num_image==1:
-                    ax[int(idx%2)].set_title(title[idx])
-                else:
-                    ax[int(idx/2), int(idx%2)].set_title(title[idx])
+                ax[int(idx/ncols), int(idx % ncols)].set_title(title[idx])
             except IndexError:
-                if ncols==1 or num_image==1:
-                    ax[int(idx%2)].set_title('Figure: {}'.format(idx))
-                else:
-                    ax[int(idx/2), int(idx%2)].set_title('Figure {}'.format(idx))
+                ax[int(idx/ncols), int(idx % ncols)
+                   ].set_title('Figure {}'.format(idx))
